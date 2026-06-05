a85574c484103e242afc3e5860b9e79301c2aa99
haycoder <yusufhammed39@gmail.com>
2026-06-05 22:59:01 +0100

    formated private key

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -9,10 +9,14 @@ let initialized = false;
 export function initFirebase(): void {
   if (initialized) return;
 
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY!.includes('\\n')
+    ? process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
+    : process.env.FIREBASE_PRIVATE_KEY!;
+
   const serviceAccount: ServiceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID!,
+    projectId:   process.env.FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    privateKey,
   };
 
   admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
